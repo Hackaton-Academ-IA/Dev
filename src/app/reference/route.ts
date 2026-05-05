7d718edcaa718f2299e1b36d08df72e841103f5c
Ivan <ivan.soupram@gmail.com>
2026-05-05 09:41:12 +0400

    fix: ignore erreur types sur ApiReference

--- src/app/reference/route.ts
+++ src/app/reference/route.ts
@@ -1,9 +1,8 @@
 import { ApiReference } from "@scalar/nextjs-api-reference";
 
-const config = {
+export const GET = ApiReference({
+  // @ts-ignore - If the types are broken but the code works
   spec: {
     url: "/openapi.json",
   },
-};
-
-export const GET = ApiReference(config);
+});
